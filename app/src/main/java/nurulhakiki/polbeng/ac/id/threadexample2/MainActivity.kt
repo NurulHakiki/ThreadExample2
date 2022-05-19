426058d1031a91a3130e705e83fe6e685da906e6
RPL-20 <nurulhakiki689@gmail.com>
2022-05-19 10:17:24 +0700

    Ini merupakan kode program Thread Example 2

---
+++ app/src/main/java/nurulhakiki/polbeng/ac/id/threadexample2/MainActivity.kt
@@ -0,0 +1,11 @@
+package nurulhakiki.polbeng.ac.id.threadexample2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
